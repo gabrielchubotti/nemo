414206d86f0a947462dce71d6dd8a8cb0532c8ee
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-03-07 21:31:46 -0300

    Initial Commit

---
+++ Nemo/NemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NemoApp.swift
+//  Nemo
+//
+//  Created by Gabriel Chu Bottiglieri on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
