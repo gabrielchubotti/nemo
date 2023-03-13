5186dbd559d322b598cf820b95ce0488a5c12705
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-03-12 22:19:27 -0300

    added BlurView file

---
+++ Nemo/View/BlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BlurView.swift
+//  Nemo
+//
+//  Created by Gabriel Chu Bottiglieri on 12/03/23.
+//
+
+import SwiftUI
+
+struct BlurView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlurView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlurView()
+    }
+}
